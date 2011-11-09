169039f54b130dec49110398be111112e5f56538
Fabricio Murai <fabricio@fabricio-laptop.(none)>
2011-11-08 21:52:57 -0500

    fabricio     - clustering using k-means: did not work very well

--- readfile_sparse.R
+++ readfile_sparse.R
@@ -1,5 +1,5 @@
 library('Matrix')
- library('irlba')
+library('irlba')
 
 tic <- function(gcFirst = TRUE, type=c("elapsed", "user.self", "sys.self"))
 {
@@ -20,7 +20,10 @@ toc <- function()
    invisible(toc)
 }
 
-data<-read.table("out3.txt");
+data<-read.table('out3.txt');
+authors<-read.table('list.txt', sep='\n')
+authors<-as.vector(authors$V1)
+
 #Jie:#data<-read.table("/home/jie/work/out3.txt");
 line_num = dim(data)[1];
 
@@ -30,4 +33,45 @@ tic()
 A = sparseMatrix(i=c(data[,1],data[,2]),j=c(data[,2],data[,1]),x=c(data[,3],data[,3]))
 toc()
 
-S = irlba(A,nu=5,nv=5)
+
+for( i in 5:10 )
+{
+tic()
+S = irlba(A,nu=i,nv=i)
+toc()
+}
+#elapsed 
+# 38.091 
+#elapsed 
+# 36.728 
+#elapsed 
+# 46.678 
+#elapsed 
+# 55.591 
+#elapsed 
+#  76.22 
+#elapsed 
+# 96.602
+#
+#For now let's take the 10 largest singular values
+#we will need to choose this number carefully later
+
+B=S$u
+
+for( c in 2:5)
+{
+    K=kmeans(B,centers=c)
+    print(K$size)
+}
+
+#It does not work very well
+#Possible reasons:
+# - I may be using a bad representation of authors, I may need to use more singular
+#values or it is just wrong to use the vectors u
+# - k-means does not work in big graphs
+# - Euclidean distance does not make sense
+
+#[1]      27 1030685
+#[1]      13 1030686      13
+#[1]      12       6       8 1030686
+#[1]       5 1030686      11       5       5
